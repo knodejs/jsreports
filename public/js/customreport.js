1f69c8d68464bd0dba52006c043271c1fe18fd63
Narongchai Thongkongkeaw <nat@gjinfotech.com>
2016-11-07 13:40:44 +0700

    init save/render designer jsreport

---
+++ public/js/customreport.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+
+    var orderData = [{
+        "userId": "1X39AN4Z92Y",
+        "userName": "John Smith",
+        "accountType": "INDIVIDUAL",
+        "orderTotal": 19.95,
+        "orderDate": "2016-02-24"
+    }, {
+        "userId": "1AC43L30HR8",
+        "userName": "Alison Jones",
+        "accountType": "BUSINESS",
+        "orderTotal": 180.50,
+        "orderDate": "2016-02-25"
+    }, {
+        "userId": "1CM499NA94R",
+        "userName": "Becky Sanderson",
+        "accountType": "BUSINESS",
+        "orderTotal": 85.00,
+        "orderDate": "2016-02-27"
+    }];
+
+    var orderSchema = {
+        fields: [{
+            name: "userId",
+            type: "text"
+        }, {
+            name: "userName",
+            type: "text"
+        }, {
+            name: "accountType",
+            type: "text"
+        }, {
+            name: "orderTotal",
+            type: "number"
+        }, {
+            name: "orderDate",
+            type: "date"
+        }]
+    };
+
+    var dataSource = {
+        id: "orders",   // Internal reference ID
+        name: "Orders",  // Data source name shown to report designer
+        data: orderData,
+        schema: orderSchema
+    };
+
+    // Create a report definition
+    var report = jsreports.createReport()
+        .data('orders')
+        .groupBy('accountType', 'accountType', 'desc')
+        .header(0.35)
+        .text('[accountType] Accounts:')
+        .footer(0.5)
+        .text('Total: [SUM(orderTotal)]', 2, 0, 2, 0.25, {
+            pattern: '$#,##0.00',
+            align: 'right',
+            bold: true
+        })
+        .detail()
+        .text('[userName]')
+        .text('[orderDate]', 1.75, 0, 1, 0.25, {
+            pattern: 'M/D/YY'
+        })
+        .text('[orderTotal]', 3, 0, 1, 0.25, {
+            pattern: '$#,##0.00',
+            align: 'right'
+        })
+        .done();
+
+    // Render the report
+    // jsreports.render({
+    //     report_def: report,
+    //     target: $('.report_preview').css('min-height', '500px'),
+    //     datasets: [dataSource]
+    // });
+
+    var data_sources = [{
+        "id": "orders",
+        "name": "Orders",
+        "data": orderData,
+        "schema": orderSchema
+    }];
+
+    var designer = new jsreports.Designer({
+        embedded: true,
+        container: $(".report-designer-container").css('min-height', '500px'),
+        data_sources: data_sources,
+        report_def: report,
+        layout: "horizontal"
+    });
+
+    $(designer).on("save", function (evt, reportdef) {
+        console.log(reportdef);
+        report_def = JSON.parse(reportdef);
+        $(".report_preview").empty();
+        jsreports.render({
+            report_def: report_def,
+            target: $(".report_preview").css('min-height', '500px'),
+            datasets: data_sources
+        });
+    });
+
+});
