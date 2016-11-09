f4cd48e2575c6a169dadea12944a0ff007e44e08
Narongchai Thongkongkongkeaw <kerlang.2515@gmail.com>
2016-11-09 20:57:53 +0700

    test move index

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 var port = process.env.PORT | 3000;
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 app.listen(port, function (res) {
     console.log("start server : http://localhost:", port);
 })
